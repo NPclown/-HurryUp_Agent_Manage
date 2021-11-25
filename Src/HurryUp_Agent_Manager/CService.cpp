9c9e64c518dec8acf063ad460e5999c7c66ba8de
DongHyun, Son <nigrumspiritus@gmail.com>
2021-11-26 03:26:44 +0900

    로그 출력 메시지 수정 및 테스트 (#5)

--- Src/HurryUp_Agent_Manager/CService.cpp
+++ Src/HurryUp_Agent_Manager/CService.cpp
@@ -4,7 +4,7 @@
 #define ENV_PATH TEXT("/var/log/hurryup/agent/env.json")
 #define ERROR_CHECK TEXT("echo $?")
 #define KILL TEXT("kill -9 ")
-#define WGET TEXT("wget -P /usr/local/bin -O HurryUp_Agent")
+#define WGET TEXT("wget -P /usr/local/bin -O /usr/local/bin/HurryUp_Agent")
 #define DELETE TEXT("rm -rf /usr/local/bin/HurryUp_Agent")
 #define CHMOD TEXT("chmod 755 /usr/local/bin/HurryUp_Agent")
 #define AGENT_PATH TEXT("/usr/local/bin/HurryUp_Agent")
@@ -29,24 +29,21 @@ void CService::Init(std::tstring _serviceName)
 	this->serviceName = _serviceName;
 }
 
-int CService::AgentInit(std::tstring data)
+std::tstring CService::AgentEnvironment(std::tstring data)
 {
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("AgentInit"));
+	core::Log_Info(TEXT("CService.cpp - [%s]"), TEXT("Setting Agent Environment Start"));
 
 	CheckDirectory(AGNET_DIRPATH);
 
 	if (data == "") {
-		core::Log_Warn(TEXT("CService.cpp - [%s]"), TEXT("Data Error"));
-		core::Log_Warn(TEXT("Data Error - [%s]"), TEXT(data.c_str()));
-		return -1;
+		core::Log_Warn(TEXT("CService.cpp - [%s]"), TEXT("Packet Data Error"));
+		return "Pacekt Data Error.";
 	}
 
-	if (AgentStop() == -1) {
-		core::Log_Warn(TEXT("CService.cpp - [%s]"), TEXT("AgentStop Error"));
-		return -1;
+	if (AgentTerminate() != "Success") {
+		core::Log_Warn(TEXT("CService.cpp - [%s]"), TEXT("Agent Stop Error"));
+		return "Agent Stop Error.";
 	}
-
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("Agent Environment Variable Init"));
 
 	ST_ENV_INFO envInfo;
 	std::tstring errMessage;
@@ -55,25 +52,25 @@ int CService::AgentInit(std::tstring data)
 
 	if (errMessage != "") {
 		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("WriteJsonToFile Error"), TEXT(errMessage.c_str()));
-		return -1;
+		return "Convert Json To File Error : " + errMessage;
 	}
 	
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("AgentInit End"));
-	return 0;
+	core::Log_Info(TEXT("CService.cpp - [%s]"), TEXT("Setting Agent Environment End"));
+	return "Success";
 }
 
-int CService::AgentStart()
+std::tstring CService::AgentExecute()
 {
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("AgentStart"));
+	core::Log_Info(TEXT("CService.cpp - [%s]"), TEXT("Agent Execute Start."));
 
-	if (AgentStop() == -1) {
+	if (AgentTerminate() != "Success") {
 		core::Log_Warn(TEXT("CService.cpp - [%s]"), TEXT("AgentStop Error"));
-		return -1;
+		return "Agent Stop Error.";
 	}
 
 	if (!core::PathFileExistsA(AGENT_PATH)) {
-		core::Log_Error(TEXT("CService.cpp - [%s]"), TEXT("Agent Not Exisit."));
-		return -1;
+		core::Log_Error(TEXT("CService.cpp - [%s]"), TEXT("Agent File Not Exisit."));
+		return "Agent File Not Exisit.";
 	}
 
 	std::tstring startCommand = "nohup " + this->serviceName + " 1> /dev/null 2>&1 &";
@@ -82,17 +79,17 @@ int CService::AgentStart()
 	std::tstring result = exec(startCommand.c_str());
 
 	if (Split(exec(ERROR_CHECK), "\n")[0] != std::tstring("0")) {
-		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error"), TEXT(result.c_str()));
-		return -1;
+		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error."), TEXT(result.c_str()));
+		return "Exec Command Error [" + result + "] : " + startCommand;
 	}
 
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("AgentStart End"));
-	return 0;
+	core::Log_Info(TEXT("CService.cpp - [%s]"), TEXT("Agent Execute End."));
+	return "Success";
 }
 
-int CService::AgentStop()
+std::tstring CService::AgentTerminate()
 {
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("AgentStop"));
+	core::Log_Info(TEXT("CService.cpp - [%s]"), TEXT("Agent Terminate Start."));
 
 	std::tstring pidCommand = "ps -eaf | grep " + this->serviceName + " | grep -v grep | grep -v HurryUp_Agent_Manager | awk '{print $2}'";
 	core::Log_Debug(TEXT("CService.cpp - [%s] : %s"), TEXT("Pid Command"), pidCommand.c_str());
@@ -100,11 +97,11 @@ int CService::AgentStop()
 	std::tstring result = exec(pidCommand.c_str());
 
 	if (result == "")
-		return 0;
+		return "Success";
 
 	if (Split(exec(ERROR_CHECK), "\n")[0] != std::tstring("0")) {
-		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error"), TEXT(result.c_str()));
-		return -1;
+		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error."), TEXT(result.c_str()));
+		return "Exec Command Error [" + result + "] : " + pidCommand;
 	}
 
 	std::tstring tmp = "";
@@ -117,59 +114,66 @@ int CService::AgentStop()
 	result = exec(killCommand.c_str());
 
 	if (Split(exec(ERROR_CHECK), "\n")[0] != std::tstring("0")) {
-		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error"), TEXT(result.c_str()));
-		return -1;
+		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error."), TEXT(result.c_str()));
+		return "Exec Command Error [" + result + "] : " + killCommand;
 	}
 
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("AgentStop End"));
-	return 0;
+	core::Log_Info(TEXT("CService.cpp - [%s]"), TEXT("Agent Terminate End."));
+	return "Success";
 }
 
-int CService::AgentUpdate(std::tstring data)
+std::tstring CService::AgentUpdate(std::tstring data)
 {
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("AgentUpdate"));
+	core::Log_Info(TEXT("CService.cpp - [%s]"), TEXT("Agent File Update Start"));
 
 	if (data == "") {
-		core::Log_Warn(TEXT("CService.cpp - [%s]"), TEXT("Data Error"));
-		core::Log_Warn(TEXT("Data Error - [%s]"), TEXT(data.c_str()));
-		return -1;
+		core::Log_Warn(TEXT("CService.cpp - [%s]"), TEXT("Packet Data Error"));
+		return "Pacekt Data Error.";
 	}
 
-	if (AgentDelete() == -1) {
-		core::Log_Warn(TEXT("CService.cpp - [%s]"), TEXT("AgentDelete Error"));
-		return -1;
+	if (AgentTerminate() != "Success") {
+		core::Log_Warn(TEXT("CService.cpp - [%s]"), TEXT("Agent Stop Error"));
+		return "Agent Stop Error.";
+	}
+
+	if (AgentDelete() != "Success") {
+		core::Log_Warn(TEXT("CService.cpp - [%s]"), TEXT("Agent Delete Error"));
+		return "Agent Delete Error.";
 	}
 	
+	//TODO :: WGET 보안 위협 제거 필요
 	std::tstring updateCommand = WGET + std::tstring(" ") + data;
 	core::Log_Debug(TEXT("CService.cpp - [%s] : %s"), TEXT("Update Command"), updateCommand.c_str());
 
 	std::tstring result = exec(updateCommand.c_str());
+	//TODO :: wget Error 잡기
 
 	if (Split(exec(ERROR_CHECK), "\n")[0] != std::tstring("0")) {
-		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error"), TEXT(result.c_str()));
-		return -1;
+		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error."), TEXT(result.c_str()));
+		return "Exec Command Error [" + result + "] : " + updateCommand;
 	}
 
 	core::Log_Debug(TEXT("CService.cpp - [%s] : %s"), TEXT("Chmod Command"), CHMOD);
 
 	result = exec(CHMOD);
+	//TODO :: chmod Error 잡기
 
 	if (Split(exec(ERROR_CHECK), "\n")[0] != std::tstring("0")) {
-		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error"), TEXT(result.c_str()));
-		return -1;
+		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error."), TEXT(result.c_str()));
+		return "Exec Command Error [" + result + "] : " + CHMOD;
 	}
 
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("AgentUpdate End"));
-	return 0;
+	core::Log_Info(TEXT("CService.cpp - [%s]"), TEXT("Agent File Update End"));
+	return "Success";
 }
 
-int CService::AgentDelete()
+std::tstring CService::AgentDelete()
 {
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("AgentDelete"));
+	core::Log_Info(TEXT("CService.cpp - [%s]"), TEXT("Agent File Delete Start"));
 
-	if (AgentStop() == -1) {
-		core::Log_Warn(TEXT("CService.cpp - [%s]"), TEXT("AgentStop Error"));
-		return -1;
+	if (AgentTerminate() != "Success") {
+		core::Log_Warn(TEXT("CService.cpp - [%s]"), TEXT("Agent Stop Error"));
+		return "Agent Stop Error.";
 	}
 
 	std::tstring deleteCommand = DELETE;;
@@ -179,17 +183,17 @@ int CService::AgentDelete()
 
 	std::tstring error = Split(exec(ERROR_CHECK), "\n")[0];
 	if (error != std::tstring("0") && error != std::tstring("127")) {
-		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error"), TEXT(result.c_str()));
-		return -1;
+		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error."), TEXT(result.c_str()));
+		return "Exec Command Error [" + result + "] : " + CHMOD;
 	}
-
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("AgentDelete End"));
-	return 0;
+	
+	core::Log_Info(TEXT("CService.cpp - [%s]"), TEXT("Agent File Delete End"));
+	return "Success";
 }
 
-int CService::AgentStatus()
+std::tstring CService::AgentStatus()
 {
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("AgentStatus"));
+	core::Log_Info(TEXT("CService.cpp - [%s]"), TEXT("Agent Status Check Start"));
 
 	std::tstring pidCommand = "ps -ax -o command | grep " + this->serviceName + " | grep -v grep | grep -v HurryUp_Agent_Manager | wc -l";
 	core::Log_Debug(TEXT("CService.cpp - [%s] : %s"), TEXT("Pid Command"), pidCommand.c_str());
@@ -199,13 +203,13 @@ int CService::AgentStatus()
 	std::cout << result << std::endl;
 
 	if (Split(result, "\n")[0] == std::tstring("0"))
-		return -1;
+		return "Dead";
 
 	if (Split(exec(ERROR_CHECK), "\n")[0] != std::tstring("0")) {
-		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error"), TEXT(result.c_str()));
-		return -1;
+		core::Log_Error(TEXT("CService.cpp - [%s] : %s"), TEXT("Exec Command Error."), TEXT(result.c_str()));
+		return "Exec Command Error [" + result + "] : " + CHMOD;
 	}
 
-	core::Log_Debug(TEXT("CService.cpp - [%s]"), TEXT("AgentStatus End"));
-	return 0;
+	core::Log_Info(TEXT("CService.cpp - [%s]"), TEXT("Agent Status Check End"));
+	return "Live";
 }
